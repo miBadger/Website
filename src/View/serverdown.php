bff2cf90b725ca4e98f9624210cabb3a8325822e
Tobias Teule <tobiasteule@gmail.com>
2018-08-23 17:27:42 +0200

    removed picture due to copyright

--- src/View/serverdown.php
+++ src/View/serverdown.php
@@ -7,9 +7,6 @@ echo View::get(__DIR__ . '/Header.php', ['title' => 'Page not found']);
   <p>
     huh, looks like something's wrong with the microverse battery. (github servers are down or have had too many requests)
   </p>
-  <div class="col-12">
-    <img class="octorick" src="\assets\images\serverdown.jpg" alt="Server down">
-  </div>
 </div>
 
 <?php echo View::get(__DIR__ . '/Footer.php'); ?>
